18380ef9d9fa1acf0c2c69f2dd835b5d3b95f547
resya202 <resyasavier@gmail.com>
2025-01-28 10:09:32 +0700

    delete all

--- anggaran_app/anggaran-app/resources/views/dashboard/pengawas.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>Dashboard Pengawas</h1>
-<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
